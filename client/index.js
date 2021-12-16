508d78c387a4533ff9af90ace2ad9b8e3b51ae28
Miles Watson <contact@mileswatson.net>
2021-12-16 14:26:51 +0000

    Added authentication to transaction requests.

--- client/index.js
+++ client/index.js
@@ -1,34 +1,57 @@
 import "./index.scss";
+import * as secp from "@noble/secp256k1";
+import * as SHA256 from "crypto-js/sha256";
 
 const server = "http://localhost:3042";
 
-document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
-  if(value === "") {
-    document.getElementById("balance").innerHTML = 0;
-    return;
-  }
+document.getElementById("exchange-address").addEventListener('input', ({ target: { value } }) => {
+	if (value === "") {
+		document.getElementById("balance").innerHTML = 0;
+		return;
+	}
 
-  fetch(`${server}/balance/${value}`).then((response) => {
-    return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
-  });
+	fetch(`${server}/balance/${value}`).then((response) => {
+		return response.json();
+	}).then(({ balance }) => {
+		document.getElementById("balance").innerHTML = balance;
+	});
 });
 
 document.getElementById("transfer-amount").addEventListener('click', () => {
-  const sender = document.getElementById("exchange-address").value;
-  const amount = document.getElementById("send-amount").value;
-  const recipient = document.getElementById("recipient").value;
+	const sender = document.getElementById("exchange-address").value;
+	const privateKey = document.getElementById("private-key").value;
+	const amount = Number(document.getElementById("send-amount").value);
+	const recipient = document.getElementById("recipient").value;
 
-  const body = JSON.stringify({
-    sender, amount, recipient
-  });
+	const publicKey = secp.getPublicKey(Uint8Array.from(Buffer.from(privateKey, 'hex')));
+	if (Buffer.from(publicKey).toString('hex') !== sender) {
+		window.alert("Private key does not match address!");
+	}
 
-  const request = new Request(`${server}/send`, { method: 'POST', body });
+	if (isNaN(amount)) window.alert("Invalid amount!");
 
-  fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
-    return response.json();
-  }).then(({ balance }) => {
-    document.getElementById("balance").innerHTML = balance;
-  });
+	const transaction = {
+		sender,
+		recipient,
+		amount: Number(amount)
+	};
+
+	const hash = SHA256(JSON.stringify(transaction)).toString();
+	secp.sign(hash, privateKey, {
+		recovered: true
+	}).then(([signature, recovery]) => {
+		const body = JSON.stringify({
+			transaction,
+			signature,
+			recovery
+		});
+
+		const request = new Request(`${server}/send`, { method: 'POST', body });
+
+		fetch(request, { headers: { 'Content-Type': 'application/json' } }).then(response => {
+			return response.json();
+		}).then(({ balance }) => {
+			document.getElementById("balance").innerHTML = balance;
+		});
+	});
 });
--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const app = express();
 const cors = require('cors');
 const port = 3042;
 
-const secp = require("noble-secp256k1");
+const secp = require("@noble/secp256k1");
+const SHA256 = require("crypto-js/sha256");
 
 // localhost can have cross origin errors
 // depending on the browser you use!
@@ -32,9 +33,17 @@ app.get('/balance/:address', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-	const { sender, recipient, amount } = req.body;
-	balances[sender] -= amount;
-	balances[recipient] = (balances[recipient] || 0) + +amount;
+	const { transaction, signature, recovery } = req.body;
+	const { sender, recipient, amount } = transaction;
+
+	if (amount >= 0 && balances[sender] >= amount) {
+		const hash = SHA256(JSON.stringify(transaction)).toString();
+		const publicKey = secp.recoverPublicKey(hash, signature, recovery);
+		if (secp.verify(signature, hash, publicKey)) {
+			balances[sender] -= amount;
+			balances[recipient] = (balances[recipient] || 0) + +amount;
+		}
+	}
 	res.send({ balance: balances[sender] });
 });
 
